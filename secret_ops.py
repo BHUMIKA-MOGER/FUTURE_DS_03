94a46ef8ec180706cc36aa2e671100f5422a8df9
BHUMIKA-MOGER <bhumikamoger16@gmail.com>
2025-11-08 09:11:34 +0530

    Added logical flaw test for bot review

--- secret_ops.py
+++ secret_ops.py
@@ -1,8 +1,8 @@
-# secret_ops.py
-
 import logging
 
 def get_user_data(user_id, password):
     # CRITICAL FLAW: Logs the unencrypted password!
     logging.info(f"Attempted login for {user_id} with password: {password}")
     return "User data..."
+
+print(hi)
