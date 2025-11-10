82d3f5d3a93912fe991399f9f6ffc59c33f5064f
BHUMIKA-MOGER <bhumikamoger16@gmail.com>
2025-11-11 01:30:39 +0530

    Added final test code (test_calc.py) for AI review

---
+++ test_calc.py
@@ -0,0 +1,7 @@
+def sum_two_numbers(a, b):
+    # This function is used to add two inputs together
+    return a + b
+
+def multiply_two_numbers(a, b):
+    # This is a new function for the bot to review!
+    return a * b
